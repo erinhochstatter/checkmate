683c0d74d0268fc3dbc7aa2b3b0b80ef290983dd
Erin Hochstatter <ehochstatter@gmail.com>
2023-03-19 13:48:50 -0500

    Edit the default migration & update schema: + migrations live in db/migrate + ran 'rails db:migrate' + new table appeared in db/schema

--- db/migrate/20230319183725_create_steps.rb
+++ db/migrate/20230319183725_create_steps.rb
@@ -1,8 +1,8 @@
 class CreateSteps < ActiveRecord::Migration[7.0]
   def change
     create_table :steps do |t|
-      t.string :name
-      t.boolean :completed
+      t.string :name, null: false
+      t.boolean :completed, default: false
 
       t.timestamps
     end
